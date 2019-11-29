8acf5a573754d341e7877c83106b90f5b540130f
markozrnic <marko.zrnic2@gmail.com>
2019-11-29 13:56:16 +0100

    LV3

---
+++ app/src/main/java/com/example/pma_lv1/PocetniActivity.java
@@ -0,0 +1,91 @@
+package com.example.pma_lv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Locale;
+
+
+public class PocetniActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Button dodajStudenta;
+    private Spinner spinner;
+    private boolean userIsInteracting;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pocetni);
+
+        dodajStudenta = findViewById(R.id.dodaj_studenta);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        dodajStudenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PersonalInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if (userIsInteracting) {
+            switch (spinner.getSelectedItemPosition()){
+                case 1:
+                    changeLanguage("hr");
+                    break;
+                case 2:
+                    changeLanguage("en");
+                    break;
+                case 3:
+                    changeLanguage("hu");
+                    break;
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    private void changeLanguage(String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+    }
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        userIsInteracting = true;
+    }
+}
