522ab76a05087bc35265b8d0ee96a51a27efe07e
Marko <marko.zrnic2@gmail.com>
2019-12-11 15:16:26 +0100

    LV5 gotov

---
+++ app/src/main/java/com/example/pma_lv1/models/CourseResponse.java
@@ -0,0 +1,12 @@
+package com.example.pma_lv1.models;
+
+import java.util.ArrayList;
+
+public class CourseResponse {
+
+    ArrayList<Course> courses;
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+}
---
+++ app/src/main/java/com/example/pma_lv1/models/Instructor.java
@@ -0,0 +1,10 @@
+package com.example.pma_lv1.models;
+
+public class Instructor {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+}
